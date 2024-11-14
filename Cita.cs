345b5537cc867937c03490089e4be46a9daba58e
jsolanoo <N00443270@upn.pe>
2024-11-14 09:09:14 -0500

    Implement CrearCita method

--- Cita.cs
+++ Cita.cs
@@ -4,9 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1
+
+public class Cita
 {
-    internal class Cita
+    // Propiedades públicas con acceso tradicional
+    public int Numero { get; set; }
+    public Estudiante Estudiante { get; set; }
+    public string Enfermedad { get; set; }
+    public double Precio { get; set; }
+
+    // Constructor tradicional
+    public Cita(int numero, Estudiante estudiante, string enfermedad, double precio)
     {
+        this.Numero = numero;
+        this.Estudiante = estudiante;
+        this.Enfermedad = enfermedad;
+        this.Precio = precio;
     }
-}
+
+    // Método ToString para mostrar información de la cita
+    public override string ToString()
+    {
+        return $"Cita #{Numero}\nEnfermedad: {Enfermedad}\nPrecio: {Precio:C}\nEstudiante: {Estudiante}";
+    }
+}   
---
+++ Estudiante.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Estudiante
+{
+    // Propiedades públicas con acceso tradicional
+    public int Codigo { get; set; }
+    public string Nombre { get; set; }
+    public string Universidad { get; set; }
+
+    // Constructor tradicional
+    public Estudiante(int codigo, string nombre, string universidad)
+    {
+        this.Codigo = codigo;
+        this.Nombre = nombre;
+        this.Universidad = universidad;
+    }
+
+    // Método ToString para mostrar información del estudiante
+    public override string ToString()
+    {
+        return $"{Nombre} (Código: {Codigo}), Universidad: {Universidad}";
+    }
+}
